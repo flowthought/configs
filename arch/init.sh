5878a33f78978b47ba3d5a45163d6acc474d8005
Kalpak Tapas <kalpak1707@gmail.com>
2017-04-14 00:40:38 +0530

    Add arch init script

---
+++ arch/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Connecting to the internet"
+wpa_supplicant -B -i wlp3s0 -c /etc/wpa_supplicant/wpa_supplicant.conf
+dhcpcd wlp3s0
+echo "\nMounting storage drive"
+mount /dev/sdc2 /mnt/storage
+echo "\nStarting xbindkeys"
+xbindkeys
