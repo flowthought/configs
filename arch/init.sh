e7a6263aad243da476a9cb15a31234b9facd7491
Kalpak Tapas <kalpak1707@gmail.com>
2017-04-14 19:02:52 +0530

    Remove xbindkeys from init

--- arch/init.sh
+++ arch/init.sh
@@ -2,7 +2,5 @@
 echo "Connecting to the internet"
 wpa_supplicant -B -i wlp3s0 -c /etc/wpa_supplicant/wpa_supplicant.conf
 dhcpcd wlp3s0
-echo "\nMounting storage drive"
+echo "Mounting storage drive"
 mount /dev/sdc2 /mnt/storage
-echo "\nStarting xbindkeys"
-xbindkeys
