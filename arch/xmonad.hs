833b4e32075808837d59777040773d4ca070ec24
Kalpak Tapas <kalpak1707@gmail.com>
2017-05-27 16:22:08 +0530

    Test xmobar togglestruts

--- arch/xmonad.hs
+++ arch/xmonad.hs
@@ -1,12 +1,27 @@
+-- Imports
 import XMonad
 import XMonad.Config
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
 
-main = xmonad $ docks def
-  { layoutHook=myLayout
-  }
 
+-- The main function. https://wiki.archlinux.org/index.php/xmonad#More_configurable
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+-- Command to launch the bar.
+myBar = "xmobar"
+
+-- Custom PP, configure it as you like. It determines what is being written to the bar.
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+-- Key binding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+-- Main configuration, override the defaults to your liking.
+myConfig = docks def
+      { layoutHook=myLayout
+      }
 
 myLayout = avoidStruts ( tiled ||| Mirror tiled ||| noBorders Full )
   where
@@ -21,5 +36,3 @@ myLayout = avoidStruts ( tiled ||| Mirror tiled ||| noBorders Full )
 
      -- Percent of screen to increment by when resizing panes
      delta = 3/100
-
-
